733a4f074377bbf80cd02d00b77bd0fb1937e4e9
Lucas Haro <lgharo06@gmail.com>
2025-10-24 12:45:29 -0300

    prototipo

--- notes-frontend/src/main.jsx
+++ notes-frontend/src/main.jsx
@@ -10,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />}>
         <Route index element={<Home />}/>
-        <Route index element={<Header />}/>
         </Route>
       </Routes>
     </BrowserRouter>
