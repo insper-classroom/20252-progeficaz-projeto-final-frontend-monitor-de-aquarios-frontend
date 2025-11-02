4c5b782c034dd3f3f7c1be0b9657bbcc85c845ce
Lucas Haro <lgharo06@gmail.com>
2025-11-01 21:26:50 -0300

    funcao BotaoPresenca implementada e funcionando com atualizacao do componente baseada nas informacoes dele

--- notes-frontend/src/components/botao_presenca/botao_aquario.jsx
+++ notes-frontend/src/components/botao_presenca/botao_aquario.jsx
@@ -1,39 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { updateOcupacao, getAquarioPorId } from "../../api/aquarioService";
 import "./botao_aquario.css";
 
+function Botao_presenca({ id }) {
+  const [aquario, setAquario] = useState(null);
 
-function Botao_presenca({ id, onUpdate }) {
+  useEffect(() => {
+		// aqui temos a carcaça padrão onde preenchemos o aquario toda vez que a 
+    async function carregarAquario() {
+      const data = await getAquarioPorId(id);
+      setAquario(data);
+    }
+    carregarAquario();
+  }, [aquario]); //aquario é passado pois queremos que o elemento atualize toda vez que ele mudar 
 
-		// pegamos o aquario no banco de dados 
-	let aquario = getAquarioPorId(id)
+	// funcao para trocar o status de ocupacao do aquario de forma assincrona 
+  const handleToggle = async () => {
+    await updateOcupacao(id);
+    const data = await getAquarioPorId(id);
+    setAquario(data);
+  };
 
-	// quando clicamos no botão de liberar ou ocupar ativamos a const handleToggle que recebe o id do aquario por meio da rota
-	const handleToggle = async () => {
-		await updateOcupacao(aquario.id);
-		onUpdate();
-	};
+	// se ainda não existir o aquario ele retorna a msg carregando 
+  if (!aquario) return <p>Carregando...</p>;
 
-
-	return (
-		<div className="card">
-			<h3>{aquario.nome || `Sala ${aquario.id}`}</h3>
-			<p><strong>Prédio:</strong> {aquario.predio}</p>
-			<p><strong>Andar:</strong> {aquario.andar}</p>
-			<p><strong>Cadeiras:</strong> {aquario.capacidade}</p>
-			<p>
-				<strong>Status:</strong>{" "}
-				<span className={aquario.ocupacao ? "ocupado" : "livre"}>
-					{aquario.ocupacao ? "Ocupado" : "Livre"}
-				</span>
-			</p>
-			<button onClick={handleToggle}>
-				{aquario.ocupacao ? "Liberar" : "Ocupar"}
-			</button>
-		</div>
-	);
+  return (
+    <div className="card">
+      <h3>{aquario.nome || `Sala ${aquario.id}`}</h3>
+      <p><strong>Prédio:</strong> {aquario.predio}</p>
+      <p><strong>Andar:</strong> {aquario.andar}</p>
+      <p><strong>Cadeiras:</strong> {aquario.capacidade}</p>
+      <p>
+        <strong>Status:</strong>{" "}
+        <span className={aquario.ocupacao ? "ocupado" : "livre"}>
+          {aquario.ocupacao ? "Ocupado" : "Livre"}
+        </span>
+      </p>
+      <button onClick={handleToggle}>
+        {aquario.ocupacao ? "Liberar" : "Ocupar"}
+      </button>
+    </div>
+  );
 }
 
 export default Botao_presenca;
-
-
--- notes-frontend/src/tela_presenca.jsx
+++ notes-frontend/src/tela_presenca.jsx
@@ -1,13 +1,17 @@
-import React, {useState} from "react";
-import './App.css'
+import React from "react";
+import { useParams } from "react-router-dom";
 import Botao_presenca from './components/botao_presenca/botao_aquario.jsx'
 
-function TelaPresenca() {
-  const [count, setCount] = useState(0)
+function Tela_presenca() {
+  const { id } = useParams(); // pega o ID da rota
 
   return (
-    <Botao_presenca/>
-  )
+    <div>
+      <h2>Tela de Presença</h2>
+			{/* passamos o id para o botao presenca para que ele faça a busca no banco com a api */}
+      <Botao_presenca id={id} />
+    </div>
+  );
 }
 
-export default TelaPresenca;
+export default Tela_presenca;
