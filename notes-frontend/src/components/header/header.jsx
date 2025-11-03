cffdcbc14314128f2bb585de3c8b40631b586be5
Arthur <arthurbzg@al.insper.edu.br>
2025-11-03 20:58:06 -0300

    header pronto

--- notes-frontend/src/components/header/header.jsx
+++ notes-frontend/src/components/header/header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
+import logoInsper from "../../assets/logoinsper.png";
 
 function Header() {
   return (
     <header className="header">
       <div className="header-content">
-        <div className="center">
-          <h1>Controle de Aquários</h1>
+        <div className="left">
+          <img src={logoInsper} alt="Insper" className="logo" />
         </div>
-
+        <div className="center">
+          <h1>Monitoramento de aquários</h1>
+        </div>
         <div className="right">
           <Link to="/login">
             <button className="login-button">Login</button>
